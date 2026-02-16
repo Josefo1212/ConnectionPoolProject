4d9524e9640f41d3f7cc9698d73a382c3c93c078
josefo1212 <jffcastillo0@gmail.com>
2026-02-16 18:39:38 -0400

    Feat:Se implemento la clase pool y la clase config

---
+++ src/Config.java
@@ -0,0 +1,27 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (var fis = new FileInputStream(".env")) { // Uso de var para simplificar la declaración
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Error al cargar el archivo .env", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static int getInt(String key) {
+        return Integer.parseInt(properties.getProperty(key));
+    }
+
+    public static long getLong(String key) {
+        return Long.parseLong(properties.getProperty(key));
+    }
+}
---
+++ src/Pool.java
@@ -0,0 +1,57 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Pool {
+    private final ArrayBlockingQueue<Connection> connectionPool;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
+
+    public Pool() throws SQLException {
+        this(
+            "jdbc:" + Config.get("DB_DIALECT") + "://" + Config.get("DB_HOST") + ":" + Config.get("DB_PORT") + "/" + Config.get("DB_NAME"),
+            Config.get("DB_USER"),
+            Config.get("DB_PASSWORD"),
+            Config.getInt("POOL_SIZE")
+        ); // Construye la URL dinámicamente desde el .env
+    }
+
+    public Pool(String dbUrl, String dbUser, String dbPassword) throws SQLException {
+        this(dbUrl, dbUser, dbPassword, Config.getInt("POOL_SIZE")); // Tamaño del pool desde el .env
+    }
+
+    public Pool(String dbUrl, String dbUser, String dbPassword, int poolSize) throws SQLException {
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+        this.connectionPool = new ArrayBlockingQueue<>(poolSize);
+
+        for (int i = 0; i < poolSize; i++) {
+            connectionPool.add(createConnection());
+        }
+    }
+
+    private Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+
+    public Connection getConnection() throws InterruptedException {
+        long timeout = Config.getLong("POOL_TIMEOUT"); // Timeout desde el .env
+        return connectionPool.poll(timeout, TimeUnit.MILLISECONDS);
+    }
+
+    public void releaseConnection(Connection connection) {
+        if (connection != null) {
+            connectionPool.offer(connection);
+        }
+    }
+
+    public void closePool() throws SQLException {
+        for (Connection connection : connectionPool) {
+            connection.close();
+        }
+    }
+}
